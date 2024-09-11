c78f9c1af6e98f55cfe7e2daaed879ade7373a63
RuthNyankum <rnyankum99@gmail.com>
2024-09-11 21:19:00 +0000

    added footer

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Navbar from '../../component/navbar';
+import Footer from '../../component/footer';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
 };
 
 export default About;
